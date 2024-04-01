27d978ecd121ddf6ecc912ef52af0647d149d54c
sreeram_9CF6556724C40968F85428D7840C0C31 <144206775+sreeramamoorthi@users.noreply.github.com>
2024-04-01 21:39:29 +0530

    Add files via upload

---
+++ simple interest.py
@@ -0,0 +1,10 @@
+
+
+p = int(input("Enter the principal amount : "))
+
+n = int(input("Enter the number of years : "))
+
+r = float(input("Enter the rate of interest :" ))
+                
+SI = (p*n*r)/100
+print("Simple Interest is ",SI)
